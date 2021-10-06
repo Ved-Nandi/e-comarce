af9bbbcac635f258a4a533e56e171b6200ad4e85
vedant <vedant.nandi@outmarch.io>
2021-10-06 21:02:20 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex', height:'100vh', justifyContent:'center', alignItems:'center', backgroundColor:'lightgray'}}>
+
+      <h1>
+        hellooooooooo
+      </h1>
+      
     </div>
   );
 }
